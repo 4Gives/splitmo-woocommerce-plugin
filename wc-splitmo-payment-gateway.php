e14dab784a5133298fa33780ce62aed431e9bdde
Kyle Aquino <kshaunaquino@gmail.com>
2024-02-15 07:06:14 +0800

    Update

--- wc-splitmo-payment-gateway.php
+++ wc-splitmo-payment-gateway.php
@@ -1,23 +1,38 @@
 <?php
-/*
-Plugin Name: Splitmo Checkout for WooCommerce
-Description: Accept payments or installments in the Philippines with Splitmo. Seamlessly integrated into WooCommerce.
-Version: 1.0.0
-Author: Splitmo
-Author URI: https://splitmo.co/
+/**
+ * Plugin Name: Splitmo Payment Gateway
+ * Plugin URI: https://splitmo.co/
+ * Description: Integration with Splitmo Payment API for WooCommerce.
+ * Version: 1.0.0
+ * Author: Splitmo Developers
+ * Author URI: https://splitmo.co/
  */
 
-defined('ABSPATH') || exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-define('WC_SPLITMO_PG_VERSION', '1.0.0');
-define('WC_SPLITMO_PG_ABSPATH', __DIR__ . '/');
+require_once plugin_dir_path(__FILE__) . 'includes/class-splitmo-payment-gateway.php';
 
-add_action('plugins_loaded', function () {
-    include_once ('includes/class-splitmo-plugin-core.php');
-});
+function init_splitmo_payment_plugin()
+{
+    if (class_exists('WC_Payment_Gateway')) {
+        function add_splitmo_payment_gateway($methods)
+        {
+            $methods[] = 'Splitmo_Payment_Gateway';
+            return $methods;
+        }
+        add_filter('woocommerce_payment_gateways', 'add_splitmo_payment_gateway');
+    }
+}
+add_action('plugins_loaded', 'init_splitmo_payment_plugin');
 
-add_filter('woocommerce_payment_gateways', function ($gateways) {
-    $gateways[] = "Splitmo_Payment_Gateway";
-    return $gateways;
-});
 
+function add_action_links($links)
+{
+    $plugin_links = array(
+        '<a href="' . admin_url('admin.php?page=wc-settings&tab=checkout&section=splitmo_payment_gateway') . '">' . __('Settings', 'splitmo-payment-plugin') . '</a>',
+    );
+    return array_merge($plugin_links, $links);
+}
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'add_action_links');
