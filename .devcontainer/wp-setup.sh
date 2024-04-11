eebafd556d7b4b3987b24cfb7265e78acf0a12c0
Jairus Mingua <minguajairus@yahoo.com.ph>
2024-04-11 23:59:34 +0800

    updates: - added debug and build scripts - added .env.examples for devcontainers - update .gitignore - update wp-setup - added .vscode preinstalled plugins for devcontainers - added configurations for php intellisense

--- .devcontainer/wp-setup.sh
+++ .devcontainer/wp-setup.sh
@@ -6,9 +6,6 @@ SITE_TITLE="Dev Site"
 #Space-separated list of plugin ID's to install and activate
 PLUGINS="woocommerce all-in-one-wp-migration"
 
-#Set to true to wipe out and reset your wordpress install (on next container rebuild)
-WP_RESET=true
-
 echo "Setting up WordPress"
 cd /var/www/html;
 
@@ -23,35 +20,45 @@ if [ ! -f wp-config.php ]; then
     echo "Configuring";
     wp config create --dbhost="$WORDPRESS_DB_HOST" --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --skip-check;
     wp core install --url="$SITE_URL" --title="$SITE_TITLE" --admin_user="$WORDPRESS_ADMIN_USERNAME" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email;
+
+    # Install Default Plugins
+    echo "Installing Default Plugins"
     wp plugin install $PLUGINS --activate
+
+    # install additional plugins added in /data/plugins
+    echo "Installing Additional Plugins"
+    cp -r /mnt/.devcontainer/data/plugins/* /var/www/html/wp-content/plugins
+    find "/mnt/.devcontainer/data/plugins/" -mindepth 1 -maxdepth 1 -type d | while read -r plugin; do
+        echo "Installing $(basename $plugin)"
+        wp plugin activate $(basename $plugin)
+    done
 
     #Create Test Product
     wp wc product create --user=admin --name="Test Product" --regular_price=500.00
 
     #Change Checkout block style to classic woocommerce
     wp post update 8 --post_content='<!-- wp:woocommerce/classic-shortcode {"shortcode":"checkout"} /-->'
-    
+
+    # wp plugin activate additional plugins
+    wp plugin activate plugin-dev
+
+    # enable woocommerce plugin-dev as payment_gateway
+    wp wc payment_gateway update splitmo_payment_gateway --enabled=1 --user=$WORDPRESS_ADMIN_USERNAME
+
     #Allow woocommerce logging
     echo "if ( ! defined( 'FS_METHOD' ) ) define( 'FS_METHOD', 'direct' );" >> wp-config.php
 
-    # # Set File Upload Limit
+    #Set File Upload Limit
     echo "@ini_set( 'upload_max_filesize' , '128M' );" >> wp-config.php
     echo "@ini_set( 'post_max_size', '128M');" >> wp-config.php
     echo "@ini_set( 'memory_limit', '256M' );" >> wp-config.php
     echo "@ini_set( 'max_execution_time', '300' );" >> wp-config.php
     echo "@ini_set( 'max_input_time', '300' );" >> wp-config.php
 
-    # wp plugin activate additional plugins
-    wp plugin install plugin-dev
-    wp plugin activate plugin-dev
-    # enable woocommerce plugin-dev as payment_gateway
-    wp wc payment_gateway update splitmo --enabled=1 --user=$WORDPRESS_ADMIN_USERNAME
-
-    # install additional plugins added in /data/plugins
-    cp -r plugins/* /var/www/html/wp-content/plugins
-    for p in plugins/*; do
-        wp plugin activate $(basename $p)
-    done
+    if [ "$WORDPRESS_DEBUG" -eq 1 ]; then
+        wp config set WP_DEBUG true --raw
+        wp config set WP_DEBUG_LOG true --raw
+    fi
 
 else
     echo "Already configured"
--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
-#!/usr/bin/env bash
-zip -r wc-splitmo-plugin.zip . -x "./build.sh" -x ".git/*" -x ".gitignore"
+#!/bin/bash
+# zips the whole plugin that can be uploaded to wordpress sites
+# Removes local development endpoint in wc-splitmo-constants.php
+
+FILE=wc-splitmo-constants.php
+
+mkdir -p ./.build && mv $FILE .build
+sed '/local/d' ./.build/$FILE > ./$FILE
+
+zip -r wc-splitmo-payment-gateway.zip . \
+    -x "*.sh" \
+    -x ".git/*" \
+    -x ".gitignore" \
+    -x ".devcontainer/*" \
+    -x ".build/*" \
+    -x ".vscode/*";
+
+mv -f ./.build/$FILE .
