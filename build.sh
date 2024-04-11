ade92fc93e138c4b84404e3fbae3a0ef4a7a91f2
Jairus Mingua <minguajairus@yahoo.com.ph>
2024-04-12 00:57:31 +0800

    update build command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # zips the whole plugin that can be uploaded to wordpress sites
-php composer.phar install
+composer install;
 zip -r wc-splitmo-payment-gateway.zip . \
     -x "*.sh" \
     -x ".git/*" \
